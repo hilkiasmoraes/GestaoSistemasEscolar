33496f35e184401a91fc0930f82cbc78bf7add10
hilkiasmoraes <hilkias.moraes@gmail.com>
2025-03-29 12:05:52 -0300

    feat: Classe Professor

---
+++ src/models/Professor.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Professor {
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+    private String idadeProfessor;
+
+    public Professor(String nomeProfessor, String disciplinaProfessor, String idadeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.disciplinaProfessor = disciplinaProfessor;
+        this.idadeProfessor = idadeProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    public String getIdadeProfessor() {
+        return idadeProfessor;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nomeProfessor='" + nomeProfessor + '\'' +
+                ", disciplinaProfessor='" + disciplinaProfessor + '\'' +
+                ", idadeProfessor='" + idadeProfessor + '\'' +
+                '}';
+    }
+}
+
